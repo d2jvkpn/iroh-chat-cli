d46ee4898541406653d86e31a51b42456da0d7ca
d2jvkpn <d2jvkpn@dev.local>
2025-06-20 22:10:50 +0800

    logging

--- src/a01.rs
+++ src/a01.rs
@@ -1,11 +1,48 @@
 use std::process;
 
+use chrono::Local;
 use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::signal;
 use tokio::time::{self, Duration};
+use tracing::{error, info, instrument, warn}; // Level
+use tracing_appender::rolling;
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::fmt::{format::Writer, time::FormatTime, writer::MakeWriterExt};
+
+struct LocalTimer;
+
+impl FormatTime for LocalTimer {
+    fn format_time(&self, w: &mut Writer<'_>) -> std::fmt::Result {
+        let now = Local::now();
+        write!(w, "{}", now.format("%Y-%m-%dT%H:%M:%S%:z"))
+    }
+}
+
+#[instrument]
+fn my_func(x: i32) {
+    info!("Running my_func with x = {}", x);
+}
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    let file_appender = rolling::daily("logs", "a01");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
+    // RUST_LOG=my_app=info,my_app::submod=debug
+    // RUST_LOG=tokio=info,my_crate=debug
+    // .with_env_filter(EnvFilter::from_default_env())
+    // with_max_level(Level::WARN)
+    tracing_subscriber::fmt()
+        .with_timer(LocalTimer)
+        .with_target(false)
+        .with_env_filter(EnvFilter::new("info"))
+        .with_writer(non_blocking.and(std::io::stdout))
+        .init();
+
+    my_func(42);
+    warn!("warning message with local time");
+    error!("this is error");
+
     let mut stdin_lines = BufReader::new(io::stdin()).lines();
 
     println!("==> Type something, or press Ctrl+C to exit.");
@@ -19,7 +56,7 @@ async fn main() -> io::Result<()> {
                match maybe_line {
                     Ok(v) => v,
                     Err(_) => {
-                      println!("!!! No input received in 60 seconds. Exiting gracefully.");
+                      warn!("!!! No input received in 60 seconds. Exiting gracefully.");
                       break;
                     }
                 }
@@ -27,19 +64,20 @@ async fn main() -> io::Result<()> {
 
             // Ctrl+C support
             _ = signal::ctrl_c() => {
-                println!("\n!!! Received Ctrl+C. Exiting.");
+                warn!("\nReceived Ctrl+C. Exiting.");
                 break;
             }
         };
 
         match line {
-            Ok(Some(v)) => println!(">>> You: {}", v),
-            Ok(None) => println!("<== End of input (EOF). Exiting."),
-            Err(e) => eprintln!("!!! Error reading input: {}", e),
+            Ok(Some(v)) => info!(">>> You: {}", v),
+            Ok(None) => info!("<== End of input (EOF). Exiting."),
+            Err(e) => error!("!!! Error reading input: {e:?}"), /* eprintln!("!!! Error reading
+                                                                 * input: {}", e), */
         }
     }
 
-    println!("<== Goodbye!");
+    info!("<== Goodbye!");
     io::stdout().flush().await?;
     process::exit(0);
     // Ok(())
