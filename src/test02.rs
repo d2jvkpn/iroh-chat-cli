c9e3dd4fdd008c9346a122da92be4c0c652bf204
d2jvkpn <d2jvkpn@dev.local>
2025-06-21 23:34:36 +0800

    ...

---
+++ src/test02.rs
@@ -0,0 +1,61 @@
+use std::io::Write;
+
+use crossterm::{
+    cursor::MoveToColumn,
+    event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
+    execute,
+    terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode},
+};
+
+fn main() -> std::io::Result<()> {
+    enable_raw_mode()?;
+
+    let mut stdout = std::io::stdout();
+    writeln!(stdout, "==> Typing, press Ctrl+C or ESC to exit")?;
+    execute!(stdout, MoveToColumn(0), Clear(ClearType::CurrentLine))?;
+    write!(stdout, "> ")?;
+    stdout.flush()?;
+
+    loop {
+        // if event::poll(std::time::Duration::from_millis(500))? {
+        //     if let Event::Key(KeyEvent { code, modifiers, kind: _, state: _ }) = event::read()? {
+        let key_event: KeyEvent = match event::read() {
+            Ok(Event::Key(v)) => v,
+            _ => continue,
+        };
+
+        match key_event.code {
+            KeyCode::Char('c') if key_event.modifiers.contains(KeyModifiers::CONTROL) => {
+                writeln!(stdout, "Ctrl+C pressed, exiting.")?;
+                break;
+            }
+            KeyCode::Esc => {
+                writeln!(stdout, "ESC pressed, exiting.")?;
+                break;
+            }
+            KeyCode::Left => write!(stdout, "LeftArrow_")?, // ←
+            KeyCode::Right => write!(stdout, "RightArrow_")?, // →
+            KeyCode::Up => write!(stdout, "UpArrow_")?,     // ↑
+            KeyCode::Down => write!(stdout, "DownArrow_")?, // ↓
+            KeyCode::Char(c) => {
+                write!(stdout, "{}", c)?;
+            }
+            KeyCode::Enter => {
+                write!(stdout, "\n")?;
+                execute!(stdout, MoveToColumn(0), Clear(ClearType::CurrentLine))?;
+                write!(stdout, "> ")?;
+            }
+            v => {
+                write!(stdout, "OtherKey::{v}_")?;
+                // execute!(stdout, MoveToColumn(0), Clear(ClearType::CurrentLine))?;
+            }
+        }
+
+        stdout.flush()?;
+    }
+    execute!(stdout, MoveToColumn(0), Clear(ClearType::CurrentLine))?;
+
+    disable_raw_mode()?;
+    println!("<== Quit");
+    Ok(())
+}
---
+++ src/test03.rs
@@ -0,0 +1,78 @@
+use std::io::Write;
+// use std::time::Duration;
+
+use crossterm::{
+    cursor::{MoveToColumn, Show},
+    event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
+    execute,
+    terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode},
+};
+
+fn main() -> std::io::Result<()> {
+    enable_raw_mode()?;
+
+    let mut stdout = std::io::stdout();
+    let mut buffer = String::new();
+    let mut cursor_pos = 0;
+    writeln!(stdout, "==> Typing, press Ctrl+C to exit")?;
+    execute!(stdout, MoveToColumn(0), Clear(ClearType::CurrentLine))?;
+
+    loop {
+        //if event::poll(Duration::from_millis(100))? {
+        //    if let Event::Key(KeyEvent { code, .. }) = event::read()? {
+        let key_event: KeyEvent = match event::read() {
+            Ok(Event::Key(v)) => v,
+            _ => continue,
+        };
+
+        match key_event.code {
+            KeyCode::Char('c') if key_event.modifiers.contains(KeyModifiers::CONTROL) => {
+                execute!(stdout, MoveToColumn(0), Clear(ClearType::CurrentLine))?;
+                writeln!(stdout, "Ctrl+C pressed, exiting.")?;
+                break;
+            }
+            KeyCode::Char(c) => {
+                buffer.insert(cursor_pos, c);
+                cursor_pos += 1;
+            }
+            KeyCode::Left => {
+                if cursor_pos > 0 {
+                    cursor_pos -= 1;
+                }
+            }
+            KeyCode::Right => {
+                if cursor_pos < buffer.len() {
+                    cursor_pos += 1;
+                }
+            }
+            KeyCode::Backspace => {
+                if cursor_pos > 0 {
+                    buffer.remove(cursor_pos - 1);
+                    cursor_pos -= 1;
+                }
+            }
+            KeyCode::Delete => {
+                if cursor_pos < buffer.len() {
+                    buffer.remove(cursor_pos);
+                }
+            }
+            KeyCode::Enter => {
+                println!("");
+                buffer.clear();
+                cursor_pos = 0;
+            }
+            _ => {}
+        }
+
+        execute!(stdout, MoveToColumn(0), Clear(ClearType::CurrentLine),)?;
+        write!(stdout, "> {}", buffer)?;
+        execute!(stdout, MoveToColumn((cursor_pos + 2) as u16))?;
+        stdout.flush()?;
+    }
+    execute!(stdout, MoveToColumn(0), Clear(ClearType::CurrentLine))?;
+    execute!(stdout, Show)?;
+
+    disable_raw_mode()?;
+    println!("<== Quit");
+    Ok(())
+}
